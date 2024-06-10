0cce1e5b00c5704c0ff010d7b8ea9f6da5948497
TechMrs <kemilat50@gmail.com>
2024-06-10 14:32:59 +0800

    testing for mobile view

--- app/ui/Component/Card/page.js
+++ app/ui/Component/Card/page.js
@@ -95,7 +95,37 @@ const Card = ({
       onDragOver={(e) => handleDragOver(e, id)}
       onDrop={(e) => handleDrop(e, id)}
     >
-      {/* Content as before */}
+     <div className="">
+         <Image
+          src={image}
+          alt={title}
+          className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-28 lg:h-28"
+          width={100}
+          height={100}
+        />
+      </div>
+
+      <div className="flex flex-col">
+        <h2 className="font-primary text-[#292B36] font-medium text-base sm:text-[1.2rem] lg:text-[1.3rem] xl:text-[1.5rem]">
+          {title}
+        </h2>
+
+        <div className="flex gap-2 items-center">
+          <div>
+            <Image
+              src={`/images/location.png`}
+              alt={title}
+              className="w-[100%] h-6 sm:w-[100%] sm:h-8 lg:w-[100%] lg:h-8"
+              width={20}
+              height={20}
+            />
+          </div>
+
+          <p className="text-[#A8A9AE] font-secondary text-lg pt-2">
+            {subLocation}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
